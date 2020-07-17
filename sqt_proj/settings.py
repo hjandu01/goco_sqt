b70e96fb71cb475d110f11d2931af553358d4596
Harminder Jandu <h.jandu@gmail.com>
2020-07-16 23:40:12 -0400

    Update settings.py

--- sqt_proj/settings.py
+++ sqt_proj/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '85h80g_^z8oc8oyap21q8zn5))&^psvh#%&7$csm359ghyoi@='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
